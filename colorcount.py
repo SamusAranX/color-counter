7e320bd2d80d377a68b8fbd4cae8b031b1716fca
Peter Wunder <hallo@peterwunder.de>
2021-07-24 05:26:54 +0200

    Added support for animated images

--- colorcount.py
+++ colorcount.py
@@ -5,7 +5,7 @@ import os
 import sys
 import argparse
 from os.path import exists, basename
-from PIL import Image
+from PIL import Image, ImageSequence, UnidentifiedImageError
 
 def int_to_rgba(i):
 	a =  i & 255
@@ -14,8 +14,7 @@ def int_to_rgba(i):
 	r = (i >> 24) & 255
 	return (r, g, b, a)
 
-def rgba_to_int(rgba):
-	r, g, b, a = rgba
+def rgba_to_int(r, g, b, a=255):
 	return (r<<24) + (g<<16) + (b<<8) + a
 
 def int_to_rgb(i):
@@ -24,47 +23,77 @@ def int_to_rgb(i):
 	r = (i >> 16) & 255
 	return (r, g, b)
 
-def rgb_to_int(rgb):
-	r, g, b = rgb
+def rgb_to_int(r, g, b):
 	return (r<<16) + (g<<8) + b
 
 def supports_truecolor():
 	colorterm = os.environ.get("COLORTERM")
 	return colorterm == "truecolor"
 
-def main(image_paths, verbose=False):
+def count_colors(img_path, count_transparent=False):
+	colors_dict = {}
+	num_frames = 0
+	try:
+		with Image.open(img_path) as img:
+			for frame in ImageSequence.Iterator(img):
+				num_frames += 1
+				w, h = frame.size
+				pix = frame.convert("RGBA").load()
+
+				for y in range(h):
+					for x in range(w):
+						r, g, b, a = pix[x, y]
+
+						if count_transparent:
+							i = rgba_to_int(r, g, b, a)
+						else:
+							if a < 128:
+								continue
+
+							i = rgb_to_int(r, g, b)
+
+						if i in colors_dict:
+							colors_dict[i] += 1
+						else:
+							colors_dict[i] = 1
+	except UnidentifiedImageError:
+		print(f"{img_path}: can't open file")
+		return None, 0
+
+	return colors_dict, num_frames
+
+def main(args):
 	colors_dict = {}
 
-	for path in image_paths:
-		if not exists(path):
-			sys.exit(f"File {path} does not exist")
+	first_item = True
 
-		with Image.open(path) as img:
-			img = img.convert("RGB")
-			pix = img.load()
-			w, h = img.size
+	for path in args.input:
+		if first_item:
+			first_item = False
+		else:
+			print()
 
-			for y in range(h):
-				for x in range(w):
-					p = pix[x, y]
-					i = rgb_to_int(p)
-					if i in colors_dict:
-						colors_dict[i] += 1
-					else:
-						colors_dict[i] = 1
+		colors_dict, num_frames = count_colors(path)
+		if not colors_dict:
+			continue
 
-	if not args.verbose:
-		print(len(colors_dict))
-	else:
-		sorted_colors = sorted(colors_dict, key=colors_dict.get)
-		for col in sorted_colors:
-			r, g, b = int_to_rgb(col)
-			amounts = colors_dict[col]
-
-			print(f"#{r:02X}{g:02X}{b:02X}: {amounts}×")
+		if not args.verbose:
+			if num_frames > 1:
+				print(f"{path}: {len(colors_dict)} colors in {num_frames} frames")
+			else:
+				print(f"{path}: {len(colors_dict)} colors")
+		else:
+			print(f"{path}:")
+			sorted_colors = sorted(colors_dict, key=colors_dict.get, reverse=True)
+			for col in sorted_colors:
+				amounts = colors_dict[col]
+				r, g, b = int_to_rgb(col)
+				print(f"#{r:02X}{g:02X}{b:02X}: {amounts}×")
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Automate ffmpeg stuff")
 	parser.add_argument("input", metavar="input", type=str, nargs="+", help="Image file(s)")
+	parser.add_argument("-t", "--include-transparency", action="store_true", help="Count 100% transparency as a color")
 	parser.add_argument("-v", "--verbose", action="store_true", help="List all colors, sorted by number of occurrences")
 	args = parser.parse_args()
+	main(args)
